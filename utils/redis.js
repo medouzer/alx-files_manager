5483bd0b92fd0ca0b50460961b842360a4e62829
mouzerzo <mouzerzo@c4r10p3.1337.ma>
2024-06-28 13:52:36 +0100

    task 0 is done

---
+++ utils/redis.js
@@ -0,0 +1,53 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+
+        this.client.on('error', (err) => {
+            console.error('Redis error:', err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, replay) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(replay);
+                }
+            })
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', duration, (err, replay) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(replay);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, replay) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(replay);
+                }
+            });
+        });
+    }
+}
+const redisClient = new RedisClient();
+module.exports = redisClient;
