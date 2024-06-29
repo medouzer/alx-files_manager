f51e116fc76120fe5f61c4066c72ee38713880de
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-29 23:22:47 +0100

    new task

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 
 class UsersController {
     static async postNew(req, res) {
@@ -17,6 +19,25 @@ class UsersController {
             });
 
             res.status(201).send({ id: user.insertedId, email });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ error: 'Server error' });
+        }
+    }
+
+    static async getMe(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) return res.status(401).send({ error: 'Unauthorized' });
+        try {
+            const key = `auth_${token}`;
+            const userId = await redisClient.get(key);
+            if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+            const collection = dbClient.db.collection('users');
+            const user = await collection.findOne({ _id: ObjectId(userId) });
+            if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+            return res.status(200).send({ id: user._id, email: user.email });
         } catch (error) {
             console.error(error);
             res.status(500).send({ error: 'Server error' });
