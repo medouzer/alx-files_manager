49f3b2953bacf3458f4f8d435778d8e39cacb5a5
ouzerzou <mohamed.ouz2016@gmail.com>
2024-07-03 00:09:20 +0100

    new projects

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,6 +3,7 @@ import redisClient from '../utils/redis';
 import { ObjectId } from 'mongodb';
 import { v4 as uuidv4 } from 'uuid';
 import { mkdir, writeFile, readFileSync } from 'fs';
+import { getMe } from './UsersController';
 
 
 class FilesController {
@@ -114,6 +115,36 @@ class FilesController {
             .toArray();
 
         return res.status(200).send(files);
+    }
+
+    static async putPublish(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+        const userId = await redisClient.get(`auth_${token}`);
+        if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+        const collection = dbClient.db.collection('files');
+        const fileId = req.params.id || '';
+        let file = await collection.findOne({ _id: ObjectId(fileId), userId: userId });
+        if (!file) return res.status(404).send({ error: 'Not found' });
+        await collection.updateOne({ _id: ObjectId(fileId) }, { $set: { isPublic: true } });
+        file = await collection.findOne({ _id: ObjectId(fileId), userId: userId });
+        return res.status(200).send(file);
+    }
+
+    static async putUnpublish(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+        const userId = await redisClient.get(`auth_${token}`);
+        if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+        const collection = dbClient.db.collection('files');
+        const fileId = req.params.id || '';
+        let file = await collection.findOne({ _id: ObjectId(fileId), userId: userId });
+        if (!file) return res.status(404).send({ error: 'Not found' });
+        await collection.updateOne({ _id: ObjectId(fileId) }, { $set: { isPublic: false } });
+        file = await collection.findOne({ _id: ObjectId(fileId), userId: userId });
+        return res.status(200).send(file);
     }
 }
 
